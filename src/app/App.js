e6945c501c3346dd7d6ee78b897110238b1bdc73
Daniel Las <lasdaniel@gmail.com>
2019-10-17 22:00:50 +0200

    Removed build folder from git index. AppBar

--- src/app/App.js
+++ src/app/App.js
@@ -9,23 +9,26 @@ import _ from 'lodash';
 
 import {createAction} from './store/actionCreators';
 import {
-  Box,
   Button,
   FormControl,
   FormControlLabel,
   FormLabel,
   Grid,
+  makeStyles,
   Radio,
   RadioGroup,
   Table,
   TableBody,
-  TableHead
+  TableHead,
+  Toolbar
 } from '@material-ui/core';
 import {Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
 import Checkbox from '@material-ui/core/Checkbox';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import {restUrlBase} from "./config/config";
+import {restUrlBase} from './config/config';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
 
 const initialState = {
   report: []
@@ -40,7 +43,14 @@ const reducer = (state = initialState, {type, payload}) => {
   }
 };
 
+const useStyles = makeStyles(theme => ({
+  offset: {
+    ...theme.mixins.toolbar,
+    flexGrow: 1
+  }
+}));
 const app = ({generateCfdReport, downloadJiraData, report}) => {
+  const classes = useStyles();
   const [interval, setInterval] = React.useState('day');
   const changeInterval = e => setInterval(e.target.value);
 
@@ -103,100 +113,111 @@ const app = ({generateCfdReport, downloadJiraData, report}) => {
   };
 
   return (
-    <Grid container spacing={5}>
-      <Grid item xs={2}>
-        <Box>
-          <FormControl component="fieldset">
-            <FormLabel>Interval: </FormLabel>
-            <RadioGroup name="cycle" value={interval} onChange={changeInterval}>
-              <FormControlLabel value="week" label="Week" control={<Radio/>}/>
-              <FormControlLabel value="day" label="Day" control={<Radio/>}/>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl component="fieldset">
-            <FormLabel>Items: </FormLabel>
-            <FormControlLabel value="Story" label="Story"
-                              control={<Checkbox onChange={changeItems} checked={items.Story}/>}/>
-            <FormControlLabel value="Bug" label="Bug" control={<Checkbox onChange={changeItems} checked={items.Bug}/>}/>
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl component="fieldset">
-            <FormLabel>Priorities: </FormLabel>
-            <FormControlLabel value="Critical" label="Critical"
-                              control={<Checkbox onChange={changePrios} checked={prios.Critical}/>}/>
-            <FormControlLabel value="Highest" label="Highest"
-                              control={<Checkbox onChange={changePrios} checked={prios.Highest}/>}/>
-            <FormControlLabel value="High" label="High"
-                              control={<Checkbox onChange={changePrios} checked={prios.High}/>}/>
-            <FormControlLabel value="Medium" label="Medium"
-                              control={<Checkbox onChange={changePrios} checked={prios.Medium}/>}/>
-            <FormControlLabel value="Low" label="Low"
-                              control={<Checkbox onChange={changePrios} checked={prios.Low}/>}/>
-            <FormControlLabel value="Lowest" label="Lowest"
-                              control={<Checkbox onChange={changePrios} checked={prios.Lowest}/>}/>
-          </FormControl>
-        </Box>
-        <Box>
-          <Button variant="contained" color="primary" onClick={() => generateCfdReport(interval, items, prios)}>
-            Generate
-          </Button>
-          <Button variant="contained" color="primary" onClick={downloadJiraData}>
-            Download data
-          </Button>
-        </Box>
-        <Box>
-          {drawChart(report)}
-        </Box>
+    <Container maxWidth={false}>
+      <AppBar position="fixed">
+        <Toolbar></Toolbar>
+      </AppBar>
+      <div className={classes.offset}/>
+      <Grid container>
+        <Grid container direction="column" xs={2} spacing={1}>
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={downloadJiraData}>
+              Download data
+            </Button>
+          </Grid>
+          <Grid item>
+            <FormControl component="fieldset">
+              <FormLabel>Interval: </FormLabel>
+              <RadioGroup name="cycle" value={interval} onChange={changeInterval}>
+                <FormControlLabel value="week" label="Week" control={<Radio/>}/>
+                <FormControlLabel value="day" label="Day" control={<Radio/>}/>
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl component="fieldset">
+              <FormLabel>Items: </FormLabel>
+              <FormControlLabel value="Story" label="Story"
+                                control={<Checkbox onChange={changeItems} checked={items.Story}/>}/>
+              <FormControlLabel value="Bug" label="Bug"
+                                control={<Checkbox onChange={changeItems} checked={items.Bug}/>}/>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl component="fieldset">
+              <FormLabel>Priorities: </FormLabel>
+              <FormControlLabel value="Critical" label="Critical"
+                                control={<Checkbox onChange={changePrios} checked={prios.Critical}/>}/>
+              <FormControlLabel value="Highest" label="Highest"
+                                control={<Checkbox onChange={changePrios} checked={prios.Highest}/>}/>
+              <FormControlLabel value="High" label="High"
+                                control={<Checkbox onChange={changePrios} checked={prios.High}/>}/>
+              <FormControlLabel value="Medium" label="Medium"
+                                control={<Checkbox onChange={changePrios} checked={prios.Medium}/>}/>
+              <FormControlLabel value="Low" label="Low"
+                                control={<Checkbox onChange={changePrios} checked={prios.Low}/>}/>
+              <FormControlLabel value="Lowest" label="Lowest"
+                                control={<Checkbox onChange={changePrios} checked={prios.Lowest}/>}/>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={() => generateCfdReport(interval, items, prios)}>
+              Generate
+            </Button>
+          </Grid>
+          <Grid item>
+            {drawChart(report)}
+          </Grid>
+        </Grid>
+        <Grid container spacing={1} xs={10}>
+          <Grid item xs={6}>
+            <ResponsiveContainer width="100%" height={800}>
+              <AreaChart data={report}>
+                <XAxis dataKey="created"/>
+                {series.todo ? <Area type="step" dataKey="To Do cumulative"/> : null}
+                {series.done ? <Area type="step" dataKey="Done cumulative"/> : null}
+                {series.ct ? <Area type="step" dataKey="CT"/> : null}
+                {series.th ? <Area type="step" dataKey="TH"/> : null}
+                <YAxis/>
+                <Tooltip/>
+              </AreaChart>
+            </ResponsiveContainer>
+          </Grid>
+          <Grid item xs={6}>
+            <div style={{overflow: 'auto', maxHeight: 800}}>
+              <Table size="small" stickyHeader={true}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Created</TableCell>
+                    <TableCell align="right">To Do</TableCell>
+                    <TableCell align="right">Done</TableCell>
+                    <TableCell align="right">WIP</TableCell>
+                    <TableCell align="right">CT</TableCell>
+                    <TableCell align="right">TH</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {
+                    report
+                      .map(r => (
+                          <TableRow>
+                            <TableCell align="center">{r.created}</TableCell>
+                            <TableCell align="right">{r['To Do cumulative']}</TableCell>
+                            <TableCell align="right">{r['Done cumulative']}</TableCell>
+                            <TableCell align="right">{r['WIP cumulative']}</TableCell>
+                            <TableCell align="right">{r['CT']}</TableCell>
+                            <TableCell align="right">{r['TH'] ? r['TH'].toFixed(2) : null}</TableCell>
+                          </TableRow>
+                        )
+                      )
+                  }
+                </TableBody>
+              </Table>
+            </div>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item xs={5}>
-        <ResponsiveContainer width="100%" height={800}>
-          <AreaChart data={report}>
-            <XAxis dataKey="created"/>
-            {series.todo ? <Area type="step" dataKey="To Do cumulative"/> : null}
-            {series.done ? <Area type="step" dataKey="Done cumulative"/> : null}
-            {series.ct ? <Area type="step" dataKey="CT"/> : null}
-            {series.th ? <Area type="step" dataKey="TH"/> : null}
-            <YAxis/>
-            <Tooltip/>
-          </AreaChart>
-        </ResponsiveContainer>
-      </Grid>
-      <Grid item xs={5}>
-        <div style={{overflow: 'auto', maxHeight: 800}}>
-          <Table size="small" stickyHeader={true}>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Created</TableCell>
-                <TableCell align="right">To Do</TableCell>
-                <TableCell align="right">Done</TableCell>
-                <TableCell align="right">WIP</TableCell>
-                <TableCell align="right">CT</TableCell>
-                <TableCell align="right">TH</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {
-                report
-                  .map(r => (
-                      <TableRow>
-                        <TableCell align="center">{r.created}</TableCell>
-                        <TableCell align="right">{r['To Do cumulative']}</TableCell>
-                        <TableCell align="right">{r['Done cumulative']}</TableCell>
-                        <TableCell align="right">{r['WIP cumulative']}</TableCell>
-                        <TableCell align="right">{r['CT']}</TableCell>
-                        <TableCell align="right">{r['TH'] ? r['TH'].toFixed(2) : null}</TableCell>
-                      </TableRow>
-                    )
-                  )
-              }
-            </TableBody>
-          </Table>
-        </div>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
 
@@ -216,10 +237,10 @@ const mapDispatchToProps = dispatch => ({
       .filter(k => prios[k])
       .reduce((a, i) => a + '&prio=' + i, '');
 
-    ajax.get(`${restUrlBase()}/cfd?interval=${interval}${itemsParams}${priosParams}`)
+    ajax.get(`${restUrlBase()}/cfd/CRYP?interval=${interval}${itemsParams}${priosParams}`)
       .subscribe(resp => dispatch(createAction('STORE_CFD_REPORT', resp.response)));
   },
-  downloadJiraData: () => ajax.post(`${restUrlBase()}/jira/download`)
+  downloadJiraData: () => ajax.post(`${restUrlBase()}/jira/CRYP/download`)
     .subscribe(resp => dispatch(createAction('JIRA_DATA_DOWNLOADED')))
 });
 
